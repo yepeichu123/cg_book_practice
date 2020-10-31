403a2e25227c4880fbb5d18cb5e5c6054d54e619
yepeichu_123 <peichu.ye@gmail.com>
2020-10-31 17:00:51 +0800

    update chap2 drawPoint

--- chapter_2/chap2_1_background.cpp
+++ chapter_2/chap2_1_background.cpp
@@ -13,7 +13,7 @@ void init(GLFWwindow* window) {
 }
 
 void display(GLFWwindow* window, double currentTime) {
-    glClearColor(1.0, 0.0, 1.0, 1.0);
+    glClearColor(1.0, 0.0, 0.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
 }
 
@@ -24,20 +24,25 @@ int main(int argc, char** argv) {
         cout << "test glfwInit...\n" << "sorry, it failed." << endl;
         exit(EXIT_FAILURE);
     }
+    // create object
+    // hint function aims to point out the opengl version
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    
-    // create object
     GLFWwindow* window = glfwCreateWindow(600, 600, "chap2_1_background", NULL, NULL);
 
-    // initialize GLEW
+    // update the connection
     glfwMakeContextCurrent(window);
+
+    // initialize GLEW
     if (glewInit() != GLEW_OK) {
         cout << "test glfwInit...\n" << "sorry, it failed." << endl;
         exit(EXIT_FAILURE);
     }
+
+    // This function sets the swap interval for the current OpenGL
     glfwSwapInterval(1);
 
+    // initialize the window
     init(window);
 
     // repeat calling display
